2362d25430a6a880dddc534cdf1008a8ac53c84e
Valeriya <wal.now@yandex.ru>
2023-04-23 20:58:27 +0300

    3

---
+++ HW3/Script.sql
@@ -0,0 +1,52 @@
+USE workshem;
+SELECT sity, sname, snum, comm
+FROM salespeople;
+
+USE workshem;
+SELECT rating, cname
+FROM customers
+WHERE sity = 'San Jose';
+
+USE workshem;
+SELECT DISTINCT snum
+FROM orderss;
+
+USE workshem;
+SELECT *
+FROM customers
+WHERE cname LIKE 'G%';
+
+USE workshem;
+SELECT *
+FROM orderss
+WHERE amt = MIN(amt);
+
+
+
+USE workshem;
+SELECT *
+FROM orderss
+WHERE amt > 1000;
+
+USE workshem;
+SELECT *
+FROM customers
+WHERE rating > 100 and sity != 'Rome';
+
+
+USE workshem;
+SELECT *
+FROM staff
+ORDER BY salary;
+
+USE workshem;
+SELECT *
+FROM staff
+ORDER BY salary DESC;
+
+
+USE workshem;
+SELECT post, SUM(salary) as total_salary 
+FROM staff
+GROUP BY post
+HAVING total_salary > 100000;
